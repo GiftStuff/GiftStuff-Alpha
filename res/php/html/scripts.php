7d419cba4d80e322777664086f6acb28e30e3427
Brandon Staab <brandonstaab@gmail.com>
2015-06-15 05:35:05 -0400

    + google/recaptcha + 404 to home

--- res/php/html/scripts.php
+++ res/php/html/scripts.php
@@ -4,3 +4,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.8.0/holder.js"></script>
 <script src='https://www.google.com/recaptcha/api.js'></script>
+
+<?php
+require_once('vendor/autoload.php');
+?>
