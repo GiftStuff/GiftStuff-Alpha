ef955a107adea3713396fcab09aecfa342de8d1a
dkeega <dkeega@gmail.com>
2015-06-25 01:51:58 -0400

    Actually for real this time it's good now because we fixed the merge because the first time we merged poorly

--- index.php
+++ index.php
@@ -3,29 +3,15 @@
 <html lang='en-US'>
     <head>
         <title>Gift Stuff - <?php echo($page); ?></title>
-	<?php
-	require_once 'res/php/html/meta.php';
-	require_once 'res/php/html/styles.php';
-	require_once 'res/php/html/scripts.php';
-	?>
+        <?php
+        require_once 'res/php/html/meta.php';
+        require_once 'res/php/html/styles.php';
+        require_once 'res/php/html/scripts.php';
+        ?>
     </head>
     <body>
-<<<<<<< HEAD
-        <header><?php include_once("res/php/header.php"); ?></header>
-        <main>
-            <?php
-            if (file_exists('page/' . $page . '.php')) {
-                include_once('page/' . $page . '.php');
-            } else {
-                include_once('res/php/404.php');
-            }
-            ?>
-        </main>
-        <footer><?php include_once("res/php/footer.php"); ?></footer>
-=======
         <header><?php require_once 'res/php/html/header.php'; ?></header>
         <main><?php require_once 'page/' . $page . '.php'; ?></main>
         <footer><?php require_once 'res/php/html/footer.php'; ?></footer>
->>>>>>> d8def143b35c00641dd787ba9eae296fdea65031
     </body>
 </html>
--- page/test.php
+++ page/test.php
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<h1  class="page-header">test</h1>
-=======
 <div class="page-header">
     <center><h1>Testing<small> a general testing page</small></h1></center>
 </div>
->>>>>>> d8def143b35c00641dd787ba9eae296fdea65031
