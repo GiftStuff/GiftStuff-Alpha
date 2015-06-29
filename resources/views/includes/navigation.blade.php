3b918caf03b71f1f8839ddd0375f1cb23c2ff3ae
Brandon Staab <brandonstaab@gmail.com>
2015-06-29 09:50:37 -0400

    active navigation page fixed

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -1,52 +1,30 @@
-<?php
-
-/**
- * @link http://getbootstrap.com/components/#glyphicons Bootstrap Glyph List
- *
- * @param string $name Name of the page to be added to navigation bar
- * @param string $location Relative URL
- * @param string $glyph [optional] A glyph from the glyph list to add an icon to the menu item
- *
- * @return string Navigation menu item
- */
-function addPage($name, $location = null, $glyph = null) {
-    global $page;
-    return sprintf('<li%s><a href="/%s">%s%s</a></li>', $location == strtolower($page) ? ' class="active"' : '', isset($location) ? $location : $name, isset($glyph) ? '<span class="glyphicon glyphicon-' . $glyph . '"></span> ' : '', $name);
-}
-?>
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                 <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">GiftStuff</a>
+            <a class="navbar-brand">GiftStuff</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
-                <?php echo(addPage('Home')); ?>
+                <li{!! Request::is( 'home') ? ' class="active"' : '' !!}><a href="/home">Home</a></li>
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">Test<span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <?php
-                        echo(addPage('1', 'test/1'));
-                        echo (addPage('1', 'test/2'));
-                        echo (addPage('3', 'test/3'));
-                        echo (addPage('4', 'test/4'));
-                        echo (addPage('5', 'test/5'));
-                        ?>
+                        <li><a href="/test/1">1</a></li>
+                        <li><a href="/test/2">1</a></li>
+                        <li><a href="/test/3">3</a></li>
+                        <li><a href="/test/4">4</a></li>
+                        <li><a href="/test/5">5</a></li>
                     </ul>
                 </li>
-                <?php
-                echo addPage('Support');
-                echo addPage('About-Us');
-                ?>
+                <li{!! Request::is( 'support') ? ' class="active"' : '' !!}><a href="/support">Support</a></li>
+                <li{!! Request::is( 'about-us') ? ' class="active"' : '' !!}><a href="/about-us">About-Us</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <?php
-                echo addPage('Sign-Up', 'signup', 'user');
-                echo addPage('Login', 'login', 'log-in')
-                ?>
+                <li{!! Request::is( 'signup') ? ' class="active"' : '' !!}><a href="/signup"><span class="glyphicon glyphicon-user"></span> Sign-Up</a></li>
+                <li{!! Request::is( 'login') ? ' class="active"' : '' !!}><a href="/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
             </ul>
         </div>
     </div>
